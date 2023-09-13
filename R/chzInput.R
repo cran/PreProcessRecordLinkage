5088fb4f667d0cf2c7cf8e17d9ade9a13088a064
Leila Marvian Mashhad <Leila.marveian@gmail.com>
2023-09-13 09:10:02 +0000

    version 1.0.1

--- R/chzInput.R
+++ R/chzInput.R
@@ -1,14 +1,15 @@
-chzInput <- function(d1, d2, chz = "NULL"){
-	pre_d <- preproc(d1, d2)
-	#ii <- pre_d$num_changed_var
-	cc <- pre_d$num_changed_var_d2
+chzInput <-
+function(d1, d2, chz = "NULL"){
+pre_d <- preproc(d1, d2)
+#ii <- pre_d$num_changed_var
+cc <- pre_d$num_changed_var_d2
 
-	pre_d$name_d2[cc[chz]] <- pre_d$name_initial2[cc[chz]]
+pre_d$name_d2[cc[chz]] <- pre_d$name_initial2[cc[chz]]
 
-	if (length(pre_d$name_initial2[cc[chz]]) == 0) {
+if (length(pre_d$name_initial2[cc[chz]]) == 0) {
           message("No variable names have been changed.")
-	    return(pre_d$name_d2)
+    return(pre_d$name_d2)
      } else {
       return(pre_d$name_d2)
-         }	
+         }
 }
--- R/selVar.R
+++ R/selVar.R
@@ -1,12 +1,13 @@
-selVar<- function(d1, d2, chz = "NULL")
+selVar <-
+function(d1, d2, chz = "NULL")
 {
-	message("please select variables from this function's output and set var1 arqument 
+message("please select variables from this function's output and set var arqument 
 in the preprocLinkage function\n")
-	dd <- create_new_data(d1, d2, chz)
-	
-	data1 <- dd$data1
-	data2 <- dd$data2
+dd <- create_new_data(d1, d2, chz)
 
-	name_int <- names(data1)  #Intersection's names
-	return(name_int)	
+data1 <- dd$data1
+data2 <- dd$data2
+
+name_int <- names(data1)  #Intersection's names
+return(name_int)
 }
